a1eed55fc0f1fd10d978a9299118e19bfcd8a648
xhwang <xhwangallen@gmail.com>
2020-06-04 21:13:18 +0800

    lengthOfLongestSubstring

---
+++ src/code2020/String/LengthOfLongestSubstring3.java
@@ -0,0 +1,40 @@
+package code2020.String;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author xhwong
+ * @date 2020/6/3
+ * @link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+public class LengthOfLongestSubstring3 {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() < 2) {
+            return s.length();
+        }
+        Set<String> strSet = new HashSet<>();
+
+        int len = 0;
+        String[] sStr = s.split("");
+        for (int i = 0; i < sStr.length; i++) {
+            String single = sStr[i];
+            if (!strSet.add(single)) {
+                i = i - strSet.size();
+                strSet = new HashSet<>();
+            }
+            if (len < strSet.size()) {
+                len = strSet.size();
+            }
+
+        }
+        return len;
+    }
+}
