a6441d10f16661d06974e0369307ef36582251e7
xhwang <xhwangallen@gmail.com>
2023-10-29 14:22:56 +0800

    2023年刷题-274

--- src/2023/HIndex274.java
+++ src/2023/HIndex274.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+
 public class HIndex274 {
+
+    public static void main(String[] args) {
+        int[] con = {1,3,1};
+        System.out.println(hIndex(con));
+    }
+
+    public static int hIndex(int[] citations) {
+        Arrays.sort(citations);
+
+        int h = 0;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            if (citations[i] > h) {
+                h++;
+            }
+        }
+
+        return h;
+
+    }
 }
